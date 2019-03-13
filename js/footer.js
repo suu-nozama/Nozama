9524b021afb62b78825735dbf2409d7caf228353
Riley Grigg <griggriley@gmail.com>
2019-03-12 23:11:27 -0600

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -1,2 +1,4 @@
 document.getElementById("foot01").innerHTML =
 "<p>March 2019 - " + new Date().getFullYear() + " Riley Grigg, others... </p> ";
+document.getElementById("email").innerHTML =
+"<p>Email us:</p> "+"<a href='mailto:griggriley@gmail.com'>Riley Grigg</a>"+"<br>";
