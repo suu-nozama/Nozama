ff4cb1bd2dc9d7ee0d3ea7f031ac4c834cc62697
Riley Grigg <griggriley@gmail.com>
2019-03-12 21:22:53 -0600

    Rename footer.js to js/footer.js

--- footer.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("foot01").innerHTML =
-"<p>March 2019 - " + new Date().getFullYear() + " Riley Grigg, others... </p> ";
---
+++ js/footer.js
@@ -0,0 +1,2 @@
+document.getElementById("foot01").innerHTML =
+"<p>March 2019 - " + new Date().getFullYear() + " Riley Grigg, others... </p> ";
