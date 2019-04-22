5c441f725cddb0e538f67be7e12a332dace40180
Riley Grigg <griggriley@gmail.com>
2019-04-22 14:53:27 -0600

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,5 @@
 document.getElementById("foot01").innerHTML =
-"<p>" + new Date().getFullYear() + " Riley Grigg, Gideon Roberts, Kara Bailey, others... </p> ";
+"<p>" + new Date().getFullYear() + " Riley Grigg, Gideon Roberts, Kara Bailey</p> ";
 
-document.getElementById("email").innerHTML =
-"<p>Email Us:</p>" + " <a target='_blank' href='mailto:griggriley@gmail.com'>Riley Grigg</a>" + "<br>" + " <a target='_blank' href='mailto:joe@joe.joe'>Joe</a>";
-
-document.getElementById("phone").innerHTML =
-"<p>Call us:</p>" + " <a href='tel:8013190923'>Riley Grigg</a>" + "<br>" + " <a href='tel:5555555555'>Joe</a>";
-
-document.getElementById("sms").innerHTML =
-"<p>Text us:</p>" + " <a href='sms:8013190923'>Riley Grigg</a>" + "<br>" + " <a href='sms:5555555555'>Joe</a>";
+document.getElementById("portfolio").innerHTML =
+"<p>Riley's Portfolio:</p>" + " <a target='_blank' href='https://sites.google.com/view/riley-grigg-edge-portfolio/home?read_current=1'>Riley Grigg</a>";
