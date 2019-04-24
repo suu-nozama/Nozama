33af0646e3431b964ecb5828889ea642e4ec6ab0
Riley Grigg <griggriley@gmail.com>
2019-04-24 15:47:34 -0600

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;" + new Date().getFullYear() + " <a target='_blank' href='https://sites.google.com/view/riley-grigg-edge-portfolio/home?read_current=1'>Riley Grigg</a>" + ", Gideon Roberts, Kara Bayn</p> ";
+"<p>&copy;" + new Date().getFullYear() + " <a target='_blank' href='https://sites.google.com/view/riley-grigg-edge-portfolio/home?read_current=1'>Riley Grigg</a>" + "<a target='_blank' href='https://edge-portfolio.firebaseapp.com/'>Gideon Roberts</a>"+ ", Kara Bayn/p> ";
 
 document.getElementById("portfolio").innerHTML =
 "<p>Riley's Portfolio:</p>" + " <a target='_blank' href='https://sites.google.com/view/riley-grigg-edge-portfolio/home?read_current=1'>Riley Grigg</a>";
