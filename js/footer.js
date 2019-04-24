c56bd990229833ec698df28ed0169c0d0e5db1f1
Riley Grigg <griggriley@gmail.com>
2019-04-24 15:15:45 -0600

    nevermind

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 var txt = "Hello World!";
 
-document.write.getElementById("foot01") =
-"<p>" + new Date().getFullYear() + txt.link("https://www.w3schools.com") + " Riley Grigg, Gideon Roberts, Kara Bayn</p> ";
+document.getElementById("foot01").innerHTML =	var txt = "Hello World!";
+"<p>" + new Date().getFullYear() + " Riley Grigg, Gideon Roberts, Kara Bayn</p> ";
 
 document.getElementById("portfolio").innerHTML =
 "<p>Riley's Portfolio:</p>" + " <a target='_blank' href='https://sites.google.com/view/riley-grigg-edge-portfolio/home?read_current=1'>Riley Grigg</a>";
